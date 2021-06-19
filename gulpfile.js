55484c0dbe7edd1f36b493ddba7380c5d8bb152a
Mel Wong <mel.wong@jud.ca.gov>
2021-06-18 22:24:46 -0700

    Add minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var clean = require('gulp-clean-css');
 var uncss = require('postcss-uncss');
 
 gulp.task('default', function () {
@@ -9,6 +10,7 @@ gulp.task('default', function () {
     })
   ];
   return gulp.src(['src/**/*.css'])
+    .pipe(clean({compatibility: 'ie11'}))
     .pipe(postcss(plugins))
     .pipe(gulp.dest('./web/css'));
 });
